f7cdd5bb7dd9f4de3a471e9bae23e8f25cafaebe
RavinderGodD <ravindergod001tbdl@gmail.com>
2024-09-05 22:54:42 +0530

    home

---
+++ script.js
@@ -0,0 +1,183 @@
+console.log("Welcome To LOP SONGS");
+
+//Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let songItemPlay = document.getElementById('songItemPlay');
+let  = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItem = Array.from(document.getElementsByClassName('songItem'));
+const background = document.querySelector('.container');
+let songs = [
+    {songName: "Bebe Bapu (Harsh Likhari)", filePath:"1.mp3", coverPath: "first.jpg"},
+    {songName: "Bexley .Road(BAGGH-E SMG x BIG KAY SMG x FARMAAN SMG)", filePath:"2.mp3", coverPath: "2.jpg"},
+    {songName: "The Last Ride (Sidhu Moose Wala)", filePath:"3.mp3", coverPath: "3.jpg"},
+    {songName: "Udda Adda (WAZIR PATAR)", filePath:"4.mp3", coverPath: "4.jpg"},
+    {songName: "Lambe Waal 2 (Gill Rohta And Jashanmeet)", filePath:"5.mp3", coverPath: "5.jpg"},
+    {songName: "Punjab (Sidhu Moose Wala)", filePath:"6.mp3", coverPath: "6.jpg"},
+    {songName: "Sapp Takkre (Pavvy Dhanjal)", filePath:"7.mp3", coverPath: "7.jpg"},
+    {songName: "Cheques (Shubh)", filePath:"8.mp3", coverPath: "8.jpg"},
+    {songName: "MVP (Shubh)", filePath:"9.mp3", coverPath: "9.jpg"},
+    {songName: "Yaaran Di Jugni(Vadda Grewal x Raka x Flop Likhari)", filePath:"10.mp3", coverPath: "10.jpg"},
+    {songName: "Dilemma (Sidhu Moose Wala)", filePath:"11.mp3", coverPath: "11.jpg"},
+    {songName: "Mehnge Ne Moti (Sidhu Moose Wala)", filePath:"12.mp3", coverPath: "12.jpg"},
+    {songName: "Same Beef (Sidhu Moose Wala)", filePath:"13.mp3", coverPath: "13.jpg"},
+    {songName: "Likhari (Arjan Dhillon)", filePath:"14.mp3", coverPath: "14.jpg"},
+    {songName: "KATH (Arjan Dhillon)", filePath:"15.mp3", coverPath: "15.jpg"},
+    {songName: "Jutti (Arjan Dhillon)", filePath:"16.mp3", coverPath: "16.jpg"},
+    {songName: "Jawani (Arjan Dhillon)", filePath:"17.mp3", coverPath: "17.jpg"},
+    {songName: "No Reason (Permish Verma)", filePath:"18.mp3", coverPath: "18.jpg"},
+    {songName: "Ohi Munde (Permish Verma)", filePath:"19.mp3", coverPath: "19.jpg"},
+    {songName: "Game Over (Karan Aujla)", filePath:"20.mp3", coverPath: "20.jpg"},
+    {songName: "Red Eye (Karan Aujla)", filePath:"21.mp3", coverPath: "21.jpg"},
+    {songName: "Jhanjar (Karan Aujla)", filePath:"22.mp3", coverPath: "22.jpg"},
+    {songName: "Rhyme Ain't Done(Navaan Sandhu Ft Sabi Bhinder / Bajwa)", filePath:"23.mp3", coverPath: "23.jpg"},
+    {songName: "East Side Flow (Sidhu Moose Wala)", filePath:"24.mp3", coverPath: "24.jpg"},
+    {songName: "Maa (Karan Aujla)", filePath:"25.mp3", coverPath: "25.jpg"},
+]
+
+
+
+songItem.forEach((element,i)=>{
+    console.log(element, i);
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
+
+//audioElement.play();
+
+//Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
+    }
+
+
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause'); 
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity = 0;
+    }
+})
+//Listen to Events
+audioElement.addEventListener('timeupdate',() =>{
+    //update seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100)
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-circle-pause');
+        element.classList.add('fa-circle-play');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        if(makeAllPlays()|| audioElement.currentTime<=0){
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause');
+        audioElement.src = `${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        (audioElement.currentTime = 0);
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-circle-play'); 
+        masterPlay.classList.add('fa-circle-pause');
+        }
+
+        else{
+        makeAllPlays()
+            songIndex = parseInt(e.target.id);
+            e.target.classList.add('fa-circle-play');
+            e.target.classList.remove('fa-circle-pause');
+            audioElement.src = `${songIndex+1}.mp3`;
+            masterSongName.innerText = songs[songIndex].songName;
+            (audioElement.currentTime = 0);
+            audioElement.pause();
+            gif.style.opacity = 0;
+            masterPlay.classList.add('fa-circle-play'); 
+            masterPlay.classList.remove('fa-circle-pause')};
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=25){
+        songIndex=0
+    }
+    else{
+        songIndex += 1;
+
+    }
+        audioElement.src = `${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play'); 
+        masterPlay.classList.add('fa-circle-pause');
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex=0
+    }
+    else{
+        songIndex -= 1;
+
+    }
+        audioElement.src = `${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play'); 
+        masterPlay.classList.add('fa-circle-pause');
+})
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function updateGradient() {
+    const color1 = getRandomColor();
+    const color2 = getRandomColor();
+    const color3 = getRandomColor();
+    
+    background.style.transition = `background 1s ease-in-out`;
+    background.style.background = `linear-gradient(45deg, ${color1}, ${color2}, ${color3})`;
+}
+
+updateGradient();
+
+setInterval(updateGradient, 10000); // Update every 5 seconds
+
+document.addEventListener('DOMContentLoaded', () => {
+    const borderContainer = document.querySelector('.border-container');
+
+    borderContainer.addEventListener('mouseover', () => {
+        borderContainer.classList.add('animate');
+    });
+
+    borderContainer.addEventListener('mouseout', () => {
+        borderContainer.classList.remove('animate');
+    });
+});
+
